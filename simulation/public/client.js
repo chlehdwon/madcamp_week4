740f8aded04f341af06d79ac8c4e310ef7f833d3
tykim5931 <tykim5931@gmail.com>
2022-07-24 22:33:06 +0900

    UI with algorithm

--- simulation/public/client.js
+++ simulation/public/client.js
@@ -1,92 +1,120 @@
 import * as THREE from 'three'
 import { OrbitControls } from './jsm/controls/OrbitControls.js'
 import { GLTFLoader } from './jsm/loaders/GLTFLoader.js'
+import { Loader } from 'three'
 import World from './world.js' 
 import Creature from './creature.js' 
 
 
-const renderer = new THREE.WebGLRenderer()
+// =============== RENDERER ===================
+const canvas = document.querySelector('#c')
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    alpha: true,
+})
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.shadowMap.enabled = true
 document.body.appendChild(renderer.domElement)
 
-
+// =============== SCENE ======================
 const scene = new THREE.Scene()
- 
+
+// =============== CAMERA =====================
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   0.1,
   1500)
-camera.position.set(0, 300, 250)
+camera.position.set(0, 370, 340)
 camera.lookAt(0,0,0)
  
+// ==================== ORBITCONTROL =========================
 const controls = new OrbitControls(camera, renderer.domElement)
- 
 
+// ===================== LIGHT ========================
+const light = new THREE.DirectionalLight(0xffffff, 1.5)
+light.position.copy( camera.position );
+light.castShadow = true
+light.receiveShadow = true
+scene.add(light)
+scene.add(light.target)
+
+// =================== PLANE =========================
 const planeGeometry = new THREE.PlaneGeometry(300,300)
 const planeMaterial = new THREE.MeshBasicMaterial({
-    color:0xFFFFFF,
+    color: 0xE4AE5B,
     side: THREE.DoubleSide
 })
 const plane = new THREE.Mesh(planeGeometry, planeMaterial)
 scene.add(plane)
 plane.rotation.x = -0.5*Math.PI
+plane.receiveShadow = true
 
-const loader = new GLTFLoader();
-const iceURL = new URL('./assets/iceground.glb', import.meta.url)
-let backgorund
-loader.load(iceURL.href, function(gltf){
-    backgorund = gltf.scene
-    backgorund.rotation
-    backgorund.scale.set(50, 10, 50)
-    backgorund.position.y = 1
-    scene.add(backgorund)
-})
+// =================== ENV GLTF (TODO: should remove and module)===
+// const loader = new GLTFLoader();
+// const iceURL = new URL('./assets/ground.glb', import.meta.url)
+// let backgorund
+// loader.load(iceURL.href, function(gltf){
+//     backgorund = gltf.scene
+//     backgorund.rotation
+//     backgorund.scale.set(50, 10, 50)
+//     backgorund.position.z = 30
+//     backgorund.position.x = 10
+//     backgorund.castShadow = true
+//     backgorund.receiveShadow = true
+//     scene.add(backgorund)
+// })
 
-const light = new THREE.PointLight(0xffffff, 2, 1500)
-light.position.set(0, 300, 250)
-scene.add(light)
 
-
-// const gridHelper = new THREE.GridHelper(500) //size of grid, division of grid
-// scene.add(gridHelper)
-
-// Here main satarts!!
-// Only one turn.....
+// ================== MAIN LOOP 1 ========================
 const myWorld = new World(scene)
-
 console.log("=====world creation done=====")
-console.log(`${myWorld.foodMap[0][0]}`)
-//create adam and eve
-let creatures = []
-myWorld.creatures.push(new Creature(1, Math.floor(Math.random() * myWorld.size.width), Math.floor(Math.random() * myWorld.size.height), scene, myWorld.size.width))
-myWorld.creatures.push(new Creature(2, Math.floor(Math.random() * myWorld.size.width), Math.floor(Math.random() * myWorld.size.height), scene, myWorld.size.width))
-myWorld.cid=3    // 생성된 크리쳐 개수
 
+//create adam and eve
+let isfarsighted = true
+for (var i =0;i<30;i++){    // 1차 소비자
+    myWorld.prey.push(new Creature(myWorld.cid++, Math.floor(Math.random() * myWorld.size.width), Math.floor(Math.random() * myWorld.size.height), scene, myWorld.size.width,1,1, isfarsighted))
+}
+for (var j =0;j<10;j++){    // 2차 소비자
+    myWorld.predator.push(new Creature(myWorld.cid++, Math.floor(Math.random() * myWorld.size.width), Math.floor(Math.random() * myWorld.size.height), scene, myWorld.size.width,3,2, isfarsighted))
+}
+
+// myWorld.creatures.push(new Creature(1, Math.floor(Math.random() * myWorld.size.width), Math.floor(Math.random() * myWorld.size.height), scene, myWorld.size.width, true))
+// myWorld.creatures.push(new Creature(2, Math.floor(Math.random() * myWorld.size.width), Math.floor(Math.random() * myWorld.size.height), scene, myWorld.size.width, true))
+// myWorld.cid=3    // 생성된 크리쳐 개수
+
+myWorld.prey.forEach((creatures)=>{
+    myWorld.creatures[creatures.position.z][creatures.position.x].push(creatures)
+})
+myWorld.predator.forEach((creatures)=>{
+    myWorld.creatures[creatures.position.z][creatures.position.x].push(creatures)
+})
 console.log("=====creature creation done=====")
 
 // create food
 myWorld.foodInit()
-
-// console.log(myWorld.foodDict)
-// console.log(myWorld.foodMap)
-// console.log(myWorld.foodMap[0][0])
-
 console.log("=====food creation done=====")
+
 
 function animate() {
     requestAnimationFrame(animate)
-    // console.log(camera.position)
+
+    // set farsighted & closesighted
+    const dist = Math.sqrt((camera.position.x*camera.position.x) + (camera.position.y*camera.position.y) + (camera.position.z*camera.position.z))
+    if(dist > 400)
+        isfarsighted = true
+    else
+        isfarsighted = false
+
     // creatures move
     if(myWorld.energy>0){
         myWorld.energy-=1
-        myWorld.step()
+        myWorld.step(isfarsighted)
     }
     else{
-        myWorld.turnOver()
+        myWorld.turnOver(isfarsighted)
         myWorld.energy=myWorld.steps
     }
-    
     render() 
 }
  
--- simulation/public/creature.js
+++ simulation/public/creature.js
@@ -3,78 +3,103 @@ import { Loader } from 'three'
 import { GLTFLoader } from './jsm/loaders/GLTFLoader.js'
 
 
-const cowURL = `assets/cow.glb`// new URL(`assets/cow.glb`, import.meta.url)
+const urlList = [`assets/nullbird.glb`, `assets/bird1.glb`, `assets/bird.glb`] // new URL(`assets/cow.glb`, import.meta.url)
+const colorList = [0x000000,0x0000FF, 0xFF0000]
 
 
 // Animation object (= Creatures)
 export default class Creature{
-    constructor(id, x, z, scene, worldSize){
-        this.position = {
-            x, z
-        }
+    constructor(id, x, z, scene, worldSize, speed, type, isfarsighted){
+        this.position = {x, z}
         this.scene = scene
         this.food = 1                  
         this.id = id                // creation id of creature
-        this.radius = 3             // radius of 3D-sphere
+        this.radius = 2          // radius of 3D-sphere
+        this.type = type
+        this.worldSize = worldSize // information of world
+        this.speed =speed
+
+        this.isfarsighted = isfarsighted
+
         this.object = null
+        this.closeView = null
+        this.farView = this.draw()
         
         this.clock = new THREE.Clock()
         this.animateMixer = null
 
-        this.init(cowURL)
-
-        this.worldSize = worldSize // information of world
+        this.init()
     } 
-    async init(url){
+    async init(){
         const loader = new GLTFLoader();
-        let gltfData = await loader.loadAsync(url)
+        let gltfData = await loader.loadAsync(urlList[this.type])
 
         gltfData.scene.position.set(this.position.x-this.worldSize/2, this.radius, this.position.z-this.worldSize/2)
-        gltfData.scene.scale.set(3,3,3)
-
-        this.animateMixer = new THREE.AnimationMixer(gltfData.scene)
-        const clips = gltfData.animations
-        const clip = THREE.AnimationClip.findByName(clips, "ArmatureAction")
-        const action = this.animateMixer.clipAction(clip);
-        action.play()
+        gltfData.scene.scale.set(this.radius,this.radius,this.radius)
+        gltfData.scene.rotation.y = Math.PI
+        gltfData.scene.castShadow = true
+        gltfData.scene.recieveShadow = true
+        // ================ DONOT ERASE!! for ANIMATION ===================
+        // this.animateMixer = new THREE.AnimationMixer(gltfData.scene)
+        // const clips = gltfData.animations
+        // const clip = THREE.AnimationClip.findByName(clips, "ArmatureAction")
+        // const action = this.animateMixer.clipAction(clip);
+        // action.play()
         
-        this.object = gltfData.scene
+        this.closeView = gltfData.scene
+        this.object = this.farView
+        if(this.isfarsighted == false){
+            this.object = this.closeView
+        }
         this.scene.add(this.object)
     }
-    // draw() {
-    //     const sphereGeometry = new THREE.SphereGeometry(this.radius)    // size, div, division of geometry
-    //     const sphereMaterial = new THREE.MeshBasicMaterial({
-    //         color: 0x0000FF,
-    //         wireframe: false
-    //     })
-    //     const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-    //     sphere.position.set(this.position.x-this.worldSize/2, this.radius, this.position.z-this.worldSize/2)
-    //     // console.log(`create sphere at ${this.position.x-15} ${this.position.z-15}`)
+    draw() {
+        const sphereGeometry = new THREE.SphereGeometry(this.radius)    // size, div, division of geometry
+    
+        const sphereMaterial = new THREE.MeshBasicMaterial({
+            color: colorList[this.type],
+            wireframe: false
+        })
+        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+        sphere.position.set(this.position.x-this.worldSize/2, this.radius, this.position.z-this.worldSize/2)
+        sphere.castShadow = true
+        sphere.receiveShadow = true
+        return sphere
+    }
+
+    update(next_x, next_z, isfarsighted) {
+        if(isfarsighted == true && this.isfarsighted == false){  
+            this.isfarsighted = true    // change to farsighted
+            this.scene.remove(this.object)
+
+            this.farView.position.x = this.object.position.x
+            this.farView.position.z = this.object.position.z
+            this.object = this.farView
+            this.scene.add(this.object)
+
+        }else if(isfarsighted == false && this.isfarsighted == true){   
+            this.isfarsighted = false   // change to closesighted
+            this.scene.remove(this.object)
+
+            this.closeView.position.x = this.object.position.x
+            this.closeView.position.z = this.object.position.z
+            this.object = this.closeView
+            this.scene.add(this.object)
+        }
         
-    //     this.scene.add(sphere)
-
-    //     return sphere
-    // }
-    // update(next_x, next_z) {
-    //     this.position.x = next_x
-    //     this.position.z = next_z
-    //     this.object.position.x = next_x - this.worldSize/2
-    //     this.object.position.z= next_z - this.worldSize/2
-    // }
-
-    update(next_x, next_z) {
         if(this.object){
             this.position.x = next_x
             this.position.z = next_z
     
-            this.animateMixer.update(this.clock.getDelta())
-            // this.object.position.set(next_x-15,0.3, next_z-15)
-            this.object.position.x = next_x - this.worldSize/2
-            this.object.position.z= next_z - this.worldSize/2
+            // this.animateMixer.update(this.clock.getDelta())
+            let x = next_x - this.worldSize/2
+            let z = next_z - this.worldSize/2
+            this.object.rotation.y = Math.atan((x - this.object.position.x)/(z - this.object.position.z)) + Math.PI
+            this.object.position.x = x
+            this.object.position.z= z
         }
     }
     destroy(){
-        
         this.scene.remove(this.object)
     }
 }
